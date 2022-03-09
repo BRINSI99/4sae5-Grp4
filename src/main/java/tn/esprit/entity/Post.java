58d99b422b30ed7e445f6c9d1974331907438343
mhaziz18 <79595573+mhaziz18@users.noreply.github.com>
2022-03-09 13:02:59 +0100

    Aziz-Commit

---
+++ src/main/java/tn/esprit/entity/Post.java
@@ -0,0 +1,45 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String subject;
+    private String content;
+    private String photo;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date DatePost;
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Comments> comments;
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Like> likes;
+
+    @ManyToOne
+    private Topic topic;
+
+
+    @ManyToOne
+    private User user;
+
+
+
+
+
+
+}
