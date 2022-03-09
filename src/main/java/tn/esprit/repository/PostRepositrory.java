4e156c01ae712c39ad616cb25ce8b7f0c631e369
mhaziz18 <79595573+mhaziz18@users.noreply.github.com>
2022-03-09 12:36:16 +0100

    Aziz-Commit

---
+++ src/main/java/tn/esprit/repository/PostRepositrory.java
@@ -0,0 +1,14 @@
+
+package tn.esprit.spring.Repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.spring.entity.Post;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+
+}
